37571d1027e8edfddb291a7a5afa9b3e5628dc08
projectssayo <projects.sayo@gmail.com>
2026-03-14 16:48:25 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from playwright.async_api import async_playwright
+
+app = FastAPI()
+
+@app.get("/get-h2")
+async def get_h2():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        page=await browser.new_page()
+        await page.goto("https://www.yr.no/en")
+        h2_texts=await page.locator("h2").all_inner_texts()
+
+        await browser.close()
+
+    return {"h2": h2_texts}
